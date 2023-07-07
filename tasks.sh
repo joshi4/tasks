6495f2f1f885716c0f47da62de1df9ed66f3997b
Shantanu <joshishantanu4@gmail.com>
2023-07-06 23:37:45 -0700

    add short-tasks; explicitly use gsed

--- tasks.sh
+++ tasks.sh
@@ -1,3 +1,32 @@
+# st: short-tasks
+# st $task adds a task to the short task list
+# st prints the first 10 short tasks
+st() {
+  if [ "$#" -eq 0 ]; then
+    head ~/.short-tasks.md | bat -l markdown
+    return 0
+  fi
+
+  if [ "$1" = "done" ]; then
+    export TASK_CONTEXT=""
+    return 0
+  fi
+
+  if [ "$1" = "ctx" ]; then
+    shift 1
+    export TASK_CONTEXT="$*"
+    return 0
+  fi
+
+
+  if [ -z "$TASK_CONTEXT" ]; then
+    echo "- $*" >> ~/.short-tasks.md
+    return 0
+  fi
+
+  gsed -i "/${TASK_CONTEXT}/a - $*" ~/.short-tasks.md
+}
+
 # lt: long-tasks
 # lt $task adds a task to the long task list
 # lt prints the first 10 long tasks
@@ -9,7 +38,7 @@ lt() {
 
   if [ "$1" = "ctx" ]; then
     shift 1
-    export TASK_CONTEXT="$@"
+    export TASK_CONTEXT="$*"
     return 0
   fi
 
@@ -19,11 +48,11 @@ lt() {
   fi
 
   if [ -z "$TASK_CONTEXT" ]; then
-    echo "- $@" >> ~/.long-tasks.md
+    echo "- $*" >> ~/.long-tasks.md
     return 0
   fi
 
-  sed -i "/${TASK_CONTEXT}/a - $*" ~/.long-tasks.md
+  gsed -i "/${TASK_CONTEXT}/a - $*" ~/.long-tasks.md
 }
 
 tasks(){
@@ -43,5 +72,3 @@ print_tasks(){
 alias vst='nvim ~/.short-tasks.md'
 alias vlt='nvim ~/.long-tasks.md'
 alias t='tasks'
-
-
